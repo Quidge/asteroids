0d709d64b5c30fb8b14aa6f7c4a207827009f4b8
Quidge <trydemirgian@gmail.com>
2017-07-17 14:07:39 -0400

    Added Vector constructor and Vector methods

--- javascript.js
+++ javascript.js
@@ -48,5 +48,16 @@ function Level() {
 	
 };
 
+// helper stuff
+function Vector(x, y) {
+	this.x = x, this.y = y;
+}
+Vector.prototype.plus = function(other) {
+	return new Vector(this.x + other.x, this.y + other.y);
+};
+Vector.prototype.times = function(factor) {
+	return new Vector(this.x * factor, this.y * factor);
+};
+
 //var game = new CanvasDisplay(document.body);
 //game.drawFrame(0);
